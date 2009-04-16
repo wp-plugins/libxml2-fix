cc6198a853e82d343f3f8a00b0c45de42602cd1f
josephscott <josephscott@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-16 03:36:10 +0000

    Bump version again to get the plugin directory to pick it up
    git-svn-id: https://plugins.svn.wordpress.org/libxml2-fix/trunk@110958 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- libxml2-fix.php
+++ libxml2-fix.php
@@ -3,7 +3,7 @@
 Plugin Name: LibXML2 Fix
 Plugin URI: http://josephscott.org/code/wordpress/plugin-libxml2-fix/
 Description: Work around for some versions of libxml2 2.7.x that strip out brackets when parsing XML.  This plugin fixes XML-RPC requests that are mangled because of this problem.  The real fix for this (making the use of this plugin unnecessary) is to use PHP 5.2.9+ with libxml2 2.7.3+.  For more information about this problem see <a href="http://core.trac.wordpress.org/ticket/7771">http://core.trac.wordpress.org/ticket/7771</a>.
-Version: 0.2.1
+Version: 0.2.2
 Author: Joseph Scott
 Author URI: http://josephscott.org/
 */
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 === LibXML2 Fix ===
 Contributors: josephscott
 Tags: xml-rpc
-Stable tag: 0.2.1
+Stable tag: 0.2.2
 
 == Description ==
 Work around for some versions of libxml2 2.7.x that strip out brackets when parsing XML.  This plugin fixes XML-RPC requests that are mangled because of this problem.  The real fix for this (making the use of this plugin unnecessary) is to use PHP 5.2.9+ with libxml2 2.7.3+.  
