2322b2427872d90379e732b9504456e867dc6758
josephscott <josephscott@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-04 15:50:18 +0000

    Enable the work around if the libxml version is below 2.7.3, instead of worrying about listing individual versions.
    Props Danilo Ercoli
    
    
    git-svn-id: https://plugins.svn.wordpress.org/libxml2-fix/trunk@447147 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- libxml2-fix.php
+++ libxml2-fix.php
@@ -3,7 +3,7 @@
 Plugin Name: LibXML2 Fix
 Plugin URI: http://josephscott.org/code/wordpress/plugin-libxml2-fix/
 Description: Work around for some versions of libxml2 2.7.x that strip out brackets when parsing XML.  This plugin fixes XML-RPC requests that are mangled because of this problem.  The real fix for this (making the use of this plugin unnecessary) is to use PHP 5.2.9+ with libxml2 2.7.3+.  For more information about this problem see <a href="http://core.trac.wordpress.org/ticket/7771">http://core.trac.wordpress.org/ticket/7771</a>.
-Version: 0.2.3
+Version: 0.2.4
 Author: Joseph Scott
 Author URI: http://josephscott.org/
 */
@@ -13,10 +13,7 @@ function jms_libxml2_fix( $methods ) {
 
 	// See http://core.trac.wordpress.org/ticket/7771
 	if ( 
-		LIBXML_DOTTED_VERSION == '2.6.27'
-		|| LIBXML_DOTTED_VERSION == '2.7.0' 
-		|| LIBXML_DOTTED_VERSION == '2.7.1' 
-		|| LIBXML_DOTTED_VERSION == '2.7.2' 
+		version_compare( LIBXML_DOTTED_VERSION, '2.7.3', '<' ) 
 		|| (
 			LIBXML_DOTTED_VERSION == '2.7.3'
 			&& version_compare( PHP_VERSION, '5.2.9', '<' )
